54274156c4825522761f8619f058c4810533b0e2
minha sohn <codeminhasohn@gmail.com>
2023-04-09 23:41:54 +0900

    [CHORE] questions 수정중

---
+++ public/javascripts/page.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button1');
+
+const nextPage = () => {
+  for (let i = 0; i < 4; i++) {
+    const { i } = req.query;
+    return i;
+  }
+};
+
+export default button.addEventListener('click', nextPage);
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,21 @@ router.get('/', (req, res) => {
   res.render('home.ejs');
 });
 
-router.get('/question', (req, res) => {
-  res.render('question.ejs');
+router.get('/question1', (req, res) => {
+  res.render('question1.ejs');
+});
+router.get('/question2', (req, res) => {
+  res.render('question2.ejs');
+});
+router.get('/question3', (req, res) => {
+  res.render('question3.ejs');
+});
+router.get('/question4', (req, res) => {
+  res.render('question4.ejs');
 });
 
-router.get('/header', (req, res) => {
-  res.render('header.ejs');
+router.get('/result', (req, res) => {
+  res.render('result.ejs');
 });
 
 export default router;
